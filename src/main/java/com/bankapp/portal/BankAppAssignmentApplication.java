5d5520a1ed24c7982571fb61b3f3f4bcd924f645
shankki_dev <skatkadeofficial@gmail.com>
2021-06-14 10:50:09 +0530

    initial commit

---
+++ src/main/java/com/bankapp/portal/BankAppAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.bankapp.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAppAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bankapp/portal/BankAppAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankapp.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
